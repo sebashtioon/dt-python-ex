f10c8a8881e755ce6c8f707f42a7f61366f47aa8
Sebastian Suciu <sebastiansuciu607@gmail.com>
2026-02-05 04:16:15 +0000

    a

--- sebashtioons-adventure.py
+++ sebashtioons-adventure.py
@@ -4,9 +4,18 @@ _in_main_menu : bool = False
 
 
 def mainMenu():
+    _in_main_menu = True
     print("--------------------")
     print("ebashtioons adventure")
     print("--------------------")
+
+    print("please select an option:")
+
+    print("\n1. play")
+    print("2. settings")
+    print("3. quit")
+
+    option = input("")
 
 def main():
     mainMenu()
