01b7a6108b86237f10053c8c1ce7efec2147a7fb
Sebastian Suciu <sebastiansuciu607@gmail.com>
2026-02-05 04:13:56 +0000

    yaay

---
+++ petly.py
@@ -0,0 +1,5 @@
+def title
+
+
+def main():
+    
---
+++ sebashtioons-adventure.py
@@ -0,0 +1,15 @@
+import time
+
+_in_main_menu : bool = False
+
+
+def mainMenu():
+    print("--------------------")
+    print("ebashtioons adventure")
+    print("--------------------")
+
+def main():
+    mainMenu()
+
+
+main()
