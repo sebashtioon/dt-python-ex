88724192cb8c231de294301b026caed1d6a51ea3
Sebastian Suciu <sebastiansuciu607@gmail.com>
2026-02-05 00:30:09 +0000

    nice

--- icecream.py
+++ icecream.py
@@ -5,14 +5,32 @@ def addIceCream(icecream):
 
 
 def main():
+
+    print("-- welcome to the ice creamery --")
+    print("------------------")
+
+    # user adds their own ice creams
     first_ice_cream = input("Enter an icecream flavour: ")
     second_ice_cream = input("Enter another icecream flavour: ")
     third_ice_cream = input("Enter yet another icecream flavour: ")
-    
     addIceCream(first_ice_cream)
     addIceCream(second_ice_cream)
     addIceCream(third_ice_cream)
 
+    # add my own
+    addIceCream("pistachio")
+    addIceCream("rocky road")
+    addIceCream("caramel")
 
+    print("------------------")
+    print("woah, those are some awesome flavours.\ni added 3 more for some more variation:")
+
+    print("------------------")
+
+    # iterate through all ice creames and print them individually
+    for ic in iceCreams:
+        print(ic)
+
+    print("------------------")
 
 main()
