308772c390e013a910fd4b55434069b1e8a25cfc
Sebastian Suciu <sebastiansuciu607@gmail.com>
2026-02-05 00:21:44 +0000

    yes

---
+++ age.py
@@ -0,0 +1,11 @@
+def main():
+    age = int(input("Enter your age: "))
+    print("---------------")
+    if age >= 18:
+        print("congrats youre an adult")
+    else:
+        print("youre a minor")
+
+
+
+main()
--- icecream.py
+++ icecream.py
@@ -1,1 +1,18 @@
-iceCreams = ["vanilla", "chocolate", "strawberry"]
+iceCreams = []
+
+def addIceCream(icecream):
+    iceCreams.append(icecream)
+
+
+def main():
+    first_ice_cream = input("Enter an icecream flavour: ")
+    second_ice_cream = input("Enter another icecream flavour: ")
+    third_ice_cream = input("Enter yet another icecream flavour: ")
+    
+    addIceCream(first_ice_cream)
+    addIceCream(second_ice_cream)
+    addIceCream(third_ice_cream)
+
+
+
+main()
